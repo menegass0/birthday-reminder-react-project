698f2a2e7b055fdb30c9e5b502025befb1a6ac9e
menegass0 <victormenegasso@unisantos.br>
2023-02-24 16:36:19 -0300

    fetching-list-of-items

--- tours-project/src/App.js
+++ tours-project/src/App.js
@@ -4,8 +4,41 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+  
+  //instead of using async and await, the correct usage of a function that returns something parallalel to the main program is to use useEffect
+  const fetchTours = async() =>{
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    }
+    catch (error){
+      setLoading(false);
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if(loading){
+    return(
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  )
 }
 
 export default App
