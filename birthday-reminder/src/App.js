88dca7774a494ce9d5302e5da0b5b50fec86394b
menegass0 <victormenegasso@unisantos.br>
2023-02-23 22:16:51 -0300

    start-of-the-project

---
+++ birthday-reminder/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+
+
+function App() {
+  const [people, setPeople] = useState();
+  return (
+    <main>
+      <section className='container'>
+        <h3>0 birthdays today</h3>
+        <List />
+        <button onClick={() =>console.log("you clicked me")}>Clear All</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
