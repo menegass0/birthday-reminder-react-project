f78ba09da661486f1cb13090207cc9a13662a52f
menegass0 <victormenegasso@unisantos.br>
2023-02-25 12:00:46 -0300

    returning-all-the-text-when-submiting

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(0)
   
-  
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setText(data);
+  }
+
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input type="number" name='amount' id='amount' value={count} onChange={(e) => setCount(e.target.value)}/>
+        <button type='submit' className='btn'>generate</button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
+  )
 }
 
 export default App;
