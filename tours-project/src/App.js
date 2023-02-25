0ec48e18acebe075b3a0c4e5af12cb4953c91321
menegass0 <victormenegasso@unisantos.br>
2023-02-24 22:13:14 -0300

    removing-tours-from-page

--- tours-project/src/App.js
+++ tours-project/src/App.js
@@ -8,8 +8,12 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(false);
   const [tours, setTours] = useState([]);
+
+  const removeTour = (id) =>{
+    const newTours =  tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
   
-  //instead of using async and await, the correct usage of a function that returns something parallalel to the main program is to use useEffect
   const fetchTours = async() =>{
     setLoading(true);
     try {
@@ -34,9 +38,19 @@ function App() {
       </main>
     )
   }
+  if(tours.length === 0){
+    return (
+    <main>
+      <div className='title'>
+        <h2>No tours left</h2>
+        <button className='btn' onClick={fetchTours}>refresh</button>
+      </div>
+    </main>
+    )
+  }
   return (
     <main>
-      <Tours tours={tours}/>
+      <Tours tours={tours} removeTour={removeTour}/>
     </main>
   )
 }
