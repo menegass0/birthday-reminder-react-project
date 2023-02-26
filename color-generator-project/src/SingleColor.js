fdac1acfbea716564f1575dde6fa07a1daaf16e9
menegass0 <victormenegasso@unisantos.br>
2023-02-26 18:54:04 -0300

    cleaning-copied-text

--- color-generator-project/src/SingleColor.js
+++ color-generator-project/src/SingleColor.js
@@ -7,9 +7,11 @@ const SingleColor = ({rgb, weight, index, hexColor}) => {
   const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`
 
-
   useEffect(()=> {
-    
+    const timeout = setTimeout(()=>{
+      setAlert(false);
+    }, 3000)
+    return () =>  clearTimeout(timeout)
   }, [alert])
   return (
     <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}} onClick={() => {
