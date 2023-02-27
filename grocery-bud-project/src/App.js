79398cb05e001d4c2ee79eceb1bb45c02ae97fe8
menegass0 <victormenegasso@unisantos.br>
2023-02-27 20:20:42 -0300

    displaying-items

--- grocery-bud-project/src/App.js
+++ grocery-bud-project/src/App.js
@@ -12,6 +12,17 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('1hrlo');
+    if(!name){
+
+    }else if(name && isEditing){
+
+    }
+    else{
+      const newItem = {id: new Date().getTime().toString(),
+      title:name,}
+      setList([...list, newItem]);
+      setName('');
+    }
   }
 
   return (
@@ -20,16 +31,18 @@ function App() {
         {alert.show && <Alert />}
         <h3>grocery bud</h3>
         <div className="form-control">
-          <input type="text" className='grocery' placeholder='e.g eggs' value={name} onChange={() => setName(e.target.value)}/>
+          <input type="text" className='grocery' placeholder='e.g eggs' value={name} onChange={(e) => setName(e.target.value)}/>
           <button type='submit' className='submit-btn'>
             {isEditing ? 'edit' : 'submit'}
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className='clear-btn'>clear items</button>
-      </div>
+      {list.length >0 && (
+        <div className="grocery-container">
+          <List items={list}/>
+          <button className='clear-btn'>clear items</button>
+        </div>
+      )}
     </section>
   )
 }
