0201be65dbe2e72067ee8219a2afe5fa9c10ff52
menegass0 <victormenegasso@unisantos.br>
2023-02-27 21:48:40 -0300

    updates-alert-timer-every-time-list-changes

--- grocery-bud-project/src/App.js
+++ grocery-bud-project/src/App.js
@@ -41,7 +41,7 @@ function App() {
   return (
     <section className="section-center">
       <form action="" className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input type="text" className='grocery' placeholder='e.g eggs' value={name} onChange={(e) => setName(e.target.value)}/>
