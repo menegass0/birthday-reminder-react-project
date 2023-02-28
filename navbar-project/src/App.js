881b608814f3758d776902b1525f2306d12961a1
menegass0 <victormenegasso@unisantos.br>
2023-02-28 16:12:15 -0300

    removing-sidevar-because-is0not-going-to-be-used

--- navbar-project/src/App.js
+++ navbar-project/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Sidebar from './Sidebar'
+
 function App() {
   return (
     <>
       <Navbar />
-      <Sidebar />
     </>
   )
 }
